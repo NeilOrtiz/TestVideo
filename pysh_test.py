dbf83e1a72e959fcbec9f1f9f6f23b56b225c06a
Ortiz Silva <nao160030@utdallas.edu>
2018-03-18 17:26:25 -0500

    msg first commimet

---
+++ pysh_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import pyshark
+
+pcap = pyshark.FileCapture(input_file="./captura1.pcap", keep_packets=True)
+
+for p in pcap:
+    p = str(p).replace('\n','')
+    p_packet = re.match(r'.*Destination:\s+(?P<dstip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3})\s+.*Source:\s+(?P<srcip>\d{1,3}[.]\d{1,3}[.]\d{1,3}[.]\d{1,3})\s+.*', p)
+    if p_packet is not None:
+        src_ip = p_packet.group('srcip')
+        dst_ip = p_packet.group('dstip')
+        print('SRC: {0:s} -> DST: {1:s}'.format(src_ip, dst_ip))
